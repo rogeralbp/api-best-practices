83efa0f73f3e1eb902a84e3d902021e009052f1a
Roger Oporta <roportaperez@gmail.com>
2022-08-15 14:15:49 -0600

    First Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Configuring Express
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const apicache = require("apicache");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+const app = express();
+
+const cache = apicache.middleware;
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use(cache("2 minutes"));
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const apicache = require("apicache");
+
+const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
+
+const router = express.Router();
+const cache = apicache.middleware;
+
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
+
+router.get("/:workoutId", workoutController.getOneWorkout);
+
+
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
+router.post("/", workoutController.createNewWorkout);
+
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+
+router.delete("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
