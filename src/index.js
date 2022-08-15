e376d48a9d15198a052754efd9b6875e801f6df0
Roger Oporta <roportaperez@gmail.com>
2022-08-15 14:32:11 -0600

    Swagger has been added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-// Configuring Express
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
-const app = express();
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
-const cache = apicache.middleware;
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
